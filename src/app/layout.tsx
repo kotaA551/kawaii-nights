0470a207d98e12bf605d2724173009e8222520ba
kotaamano <kotaamano@outlook.com>
2025-10-15 00:02:36 +0900

    privacy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 import Header from "@/components/Header";
 
 export const metadata: Metadata = {
@@ -11,7 +12,7 @@ export const metadata: Metadata = {
     initialScale: 1,
     maximumScale: 1,
     userScalable: false,
-    viewportFit: "cover", // iOS セーフエリア対応
+    viewportFit: "cover", 
   },
 };
 
@@ -19,6 +20,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="min-h-dvh bg-white text-zinc-800 antialiased">
+         <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-11S0SPSD9L"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-11S0SPSD9L');
+          `}
+        </Script>
         <Header />
         <main className="mx-auto max-w-screen-xl px-4 pb-[max(env(safe-area-inset-bottom),8px)]">
           {children}
